93ab614d21343882fc18ec4c4ec3d097a2ea8c13
coletrain9903 <coleboggan@hotmail.com>
2019-11-13 23:22:20 -0500

    New FP Player Asset and Holding Script
    - Imported standard assets to Unityj
    - Added PickUp script for holding objects marked with 'Pickup' tag

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Variables
+
+    // Public
+    public GameObject player;
+    public GameObject pickUp;
+    public GameObject playerCamera;
+    public float grabDist;
+    public string pickupTag;
+
+    // Private
+    private Vector3 oldPosition;
+    private Vector3 position;
+    private Vector3 forward;
+
+    private RaycastHit hit;
+
+    private int counter;
+    private float holdDist;
+    private bool holding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        holding = false;
+        counter = 0;
+        pickupTag = "Pickup";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        forward = playerCamera.transform.forward;
+
+        Color _color = new Color(0, 0, 20.0f);
+        Debug.DrawLine(player.transform.position, 10 * (player.transform.position + player.transform.forward), _color);
+
+        Color _color2 = new Color(1.0f, 0, 0);
+        Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + (2 * forward), _color2);
+
+        Color _color3 = new Color(0, 1.0f, 0);
+        Debug.DrawLine(pickUp.transform.position, playerCamera.transform.position + (2 * forward), _color3);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            /*if ((pickUp.transform.position - (playerCamera.transform.position + (2 * forward))).magnitude < 2 * pickUp.GetComponent<pickUpCollider>().radius)
+            {
+                Debug.Log("Pick Up");
+
+                holding = !holding;
+
+                if (!holding)
+                {
+                    Vector3 moveVec = -oldPosition + position;
+                    pickUp.GetComponent<Rigidbody>().velocity = moveVec;
+                }
+            }
+            else
+            {
+                Debug.Log("Don't Pick Up");
+            }*/
+
+            // Check for raycast within the player's grab distance
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, grabDist) && !holding)
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+                if (hit.collider.gameObject.tag == pickupTag)
+                {
+                    holding = true;
+
+                    pickUp = hit.collider.gameObject;
+                    pickUp.GetComponent<Rigidbody>().isKinematic = true;
+
+                    oldPosition = pickUp.transform.position;
+
+                    holdDist = hit.distance;
+                }
+                else
+                {
+                    Debug.Log("Not Pick-up-able");
+                }
+            }
+            else if (holding)
+            {
+                Vector3 moveVec = -oldPosition + position;
+                pickUp.GetComponent<Rigidbody>().isKinematic = false;
+                pickUp.GetComponent<Rigidbody>().velocity = moveVec;
+
+                holding = false;
+
+                Debug.Log("Put Down");
+            }
+            else
+            {
+                Debug.Log("No Hit");
+            }
+        }
+
+        if (holding)
+        {
+            position = player.transform.position + (holdDist * forward);
+            position.y += 0.5f;
+            pickUp.GetComponent<Rigidbody>().MovePosition(position);
+            pickUp.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            // OLD TESTS/ OTHER WAYS TO DO MOVEMENT
+            //pickUp.transform.position = position;
+
+            //Vector3 moveVec = -oldPosition + position;
+
+            //pickUp.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //pickUp.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+
+        if (counter == 20)
+        {
+            oldPosition = position;
+            counter = 0;
+        }
+
+        counter++;
+    }
+}
