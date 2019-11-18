8446a365bee60839181583efa7d95d46a044c87e
ggc2903 <ggc2903@IGM2025-15>
2019-11-18 16:29:31 -0500

    Added GraysonTest scene and IngredientSpawner script to spawn basic ingredients

---
+++ Assets/Scripts/IngredientSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+    //A variable to hold a copy of the lime prefab for easy access
+    public GameObject limePrefab;
+
+    //A list of GameObjects, each one placed in the scene so they hold specified positions in the scene where incredients can be spawned 
+    public List<GameObject> positionsToSpawnAt;
+
+    //A hardcoded list of spawnable ingredient prefabs to test if method is working
+    public List<GameObject> listOfSpawnables;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnIngredients(listOfSpawnables, positionsToSpawnAt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Method to spawn ingredients at start of level in positions based on ingredients needed in level 
+    void SpawnIngredients(List<GameObject> requiredIngredients, List<GameObject> positions)
+    {
+        for (int i = 0; i < requiredIngredients.Count; i++)
+        {
+            Instantiate(requiredIngredients[i], positions[i].transform.position, Quaternion.identity);
+        }
+    }
+}
