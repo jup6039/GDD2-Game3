96295624e02fa757779bf61fcc32a531dc91d077
jup6039 <42753442+jup6039@users.noreply.github.com>
2019-11-18 19:27:24 -0500

    Created UI test scene with beginnings of objective tracker and game over menu to restart game

---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RestartScene : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0); // loads current scene
+        Debug.Log("scene restarted");
+    }
+
+}
