f6fbe56b8a1aa8f29189d1ea3598e776e01a04e9
jup6039 <42753442+jup6039@users.noreply.github.com>
2019-11-25 21:25:54 -0500

    Tried to get buttons to work, they didn't, so now just key commands

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("clicked!");
+        SceneManager.LoadScene("Level1");
+    }
+}
