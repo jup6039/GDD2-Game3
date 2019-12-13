66435b514885d4e6f933cf5d92188763fbf73385
Raman Mandavia <rcm4155@rit.edu>
2019-12-13 12:25:20 -0500

    Laptop suffering from Lime Overload, need to switch to lab comp

---
+++ Assets/Scripts/FridgeMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeMovement : MonoBehaviour
+{
+    //Variables
+    public float xMovement;
+    public float yMovement;
+    public float zMovement; // These 3 are how much each frame the object will move
+    public float reverseTimer; // The timer that determines when the movement of the object will reverse
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= reverseTimer)
+        {
+            timer = 0;
+            xMovement = -xMovement;
+            yMovement = -yMovement;
+            zMovement = -zMovement;
+        }
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + (xMovement * Time.deltaTime),
+            gameObject.transform.position.y + (yMovement * Time.deltaTime), 
+            gameObject.transform.position.z + (zMovement * Time.deltaTime));
+    }
+}
